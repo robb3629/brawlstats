8134d8a46ee0be8b78af31c4dec0ec1823ae4596
robb3629 <163163040+robb3629@users.noreply.github.com>
2024-05-05 22:30:48 +0200

    spero dio muoia

--- src/commands/BrawlStats/qualcunomidecapiti.js
+++ src/commands/BrawlStats/qualcunomidecapiti.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js")
-// const { API_KEY } = require('../../../config.json')
-// const url = `https://api.brawlstars.com/v1/players/%23{Option}`
+const { API_KEY } = require('../../../config.json')
+const url = `https://api.brawlstars.com/v1/players/%23`
 
 module.exports={
     data: new SlashCommandBuilder()
@@ -12,10 +12,23 @@ module.exports={
                     .setRequired(true)
         ),
     async execute(interaction) {
-        const playertag = interaction.options.get("playertag");
-        // if (tag.startsWith("#")) {
-        //     tag.replace("#", "")
-        // }
-        await interaction.reply(playertag)
+        const playertag = interaction.options.getString("playertag") ?? "No playertag given";
+        if (playertag.startsWith("#")) {
+            playertag.replace("#", "")
+        }
+        const diocane = url + playertag
+        fetch(diocane,{
+            headers: {
+              Authorization: `Bearer ${API_KEY}`,
+            }
+        }
+    )
+
+        .then(response => response.json())
+        // .then(data => console.log(data))
+        .catch(error => console.log(error)
+    )
+    
+        await interaction.reply(response.json["trophies"])
     }
 }
