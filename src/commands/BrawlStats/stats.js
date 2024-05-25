44b8caf8f14b0121f616ee43a392852cf156a832
rob <robb29007@gmail.com>
2024-05-25 19:48:00 +0200

    whattesigma

--- src/commands/BrawlStats/stats.js
+++ src/commands/BrawlStats/stats.js
@@ -1,43 +1,40 @@
-const {ApplicationCommandOptionType, EmbedBuilder, Colors} = require('discord.js')
+const {SlashCommandBuilder, EmbedBuilder} = require('discord.js')
 const edge = require('../../oreo/edge')
 
-module.exports={
-    name: 'stats',
-    description: 'This command shows you your statistics',
-    options: [
-        {
-            name : "playertag",
-            description: "Enter player tag",
-            required : true,
-            type : ApplicationCommandOptionType.String,
-        }
-    ],
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('stats')
+        .setDescription('This command shows you your statistics')
+        .addStringOption(option => option
+            .setName('playertag')
+            .setDescription('Enter player tag')
+            .setRequired(true)
+        ),
 
-    callback: async (client, interaction) => {
-        const playertag = interaction.options.get("playertag").value
-        if (playertag.startsWith("#")) {playertag.replace("#", "")}
-        
-        const gattino = await edge(playertag, interaction)
+     async execute(client, interaction) {
+        const playertag = interaction.options.getString('playertag')
+        const tag = playertag.startsWith('#') ? playertag.slice(1) : playertag;
+        const gattino = await edge(tag, interaction)
         const color = gattino.nameColor
         var hex = "#"+color.slice(4)
-
+        
         const embed = new EmbedBuilder()
             .setTitle(`${gattino.name}`)
             .setColor(hex)
-            .setDescription(`Trophies: ${gattino.trophies}
-Solo victories: ${gattino.soloVictories}
-Duo victories: ${gattino.duoVictories}
-Trio victories: ${gattino['3vs3Victories']}
-Club: ${gattino.club.name}`)
+            .setThumbnail(`https://cdn-old.brawlify.com/profile/${gattino.icon.id}.png`)
+            .addFields(
+                {name: 'Trophies', value: `${gattino.trophies}`, inline: true},
+                {name: 'Highest trophies', value: `${gattino.highestTrophies}`, inline: true},
+            )
             .setFooter({text: `${gattino.tag}`})
+            if (gattino.club.name) {embed.addFields({name: 'Club', value: `${gattino.club.name}`, inline: true})}
+            embed.addFields(
+                { name: '\u200B', value: '\u200B' },
+                {name: 'Solo victories', value: `${gattino.soloVictories}`, inline: true},
+                {name: 'Duo victories', value: `${gattino.duoVictories}`, inline: true},
+                {name: 'Trio victories', value: `${gattino['3vs3Victories']}`, inline: true}
+            )
 
         interaction.reply({embeds : [embed]})
-
-
-
-
-
-
-
     }
 }
--- src/events/interactionCreate/handleCommands.js
+++ src/events/interactionCreate/handleCommands.js
@@ -1,68 +1,21 @@
-const {devserverId, devs} = require("../../config.json")
-const getLocalCommands = require("../../oreo/getLocalCommands")
-
-
-
-
 module.exports = async (client, interaction) => {
-    if (!interaction.isChatInputCommand()) return   
-    const localCommands = getLocalCommands()
+    if (!interaction.isChatInputCommand()) return;
 
-    try {
-        const commandObject = localCommands.find((cmd) => cmd.name === interaction.commandName)
+		const command = interaction.client.commands.get(interaction.commandName);
 
-        if (!commandObject) return
+		if (!command) {
+			console.log(`No command matching ${interaction.commandName} was found.`);
+			return;
+		}
 
-        if (commandObject.devonly) {
-            if (!devs.includes(interaction.member.id)) {
-                interaction.reply({
-                    content: `You can't run this command`,
-                    ephemeral: true
-                })
-                return
-            }
-        }
-
-        if (commandObject.testOnly) {
-            if (!(interaction.guild.id === devserverId)) {
-                interaction.reply({
-                    content: `You can't run this command here`,
-                    ephemeral: true
-                })
-                return
-            }
-        }
-
-            if (commandObject.permissionsRequired?.lenght) {
-                for (const permission of commandObject.permissionsRequired) {
-                    if (!interaction.member.permissions.has(permission)) {
-                        interaction.reply({
-                            content: "You don't have the required permissions",
-                            ephemeral: true,
-                        })
-                        return
-                    }
-                }
-            }
-
-            if (commandObject.botPermissions?.lenght) {
-                for (const permission of commandObject.botPermissions) {
-                    const bot = interaction.guild.members.me 
-                    if (!bot.permissions.has(permission)) {
-                        interaction.reply({
-                            content:`I don't have enough permissions`,
-                            ephemeral: true,
-                        })
-                        return
-                    }
-                }
-            }
-
-            await commandObject.callback(client, interaction)
-
-
-        
-    } catch (error) {
-        console.log(`There was an error while executing the command: ${error}`)
-    }
-}
+		try {
+			await command.execute(client, interaction);
+		} catch (error) {
+			console.log(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+			} else {
+				await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+			}
+		}
+};
